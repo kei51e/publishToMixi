b691e05d9e451c5fb280c78f9bedc879b00e634a
Kei Saito <kei.saito@gmail.com>
2009-05-28 23:08:38 -0700

    Add testcase

--- test/TestJpegExtraction.php
+++ test/TestJpegExtraction.php
@@ -8,10 +8,16 @@ class TestJpegExtraction extends Test {
 	}
 
 	function testExtractJpegImages () {
-		$html = '<html><body><img src="http://farm4.static.flickr.com/3409/3564522132_9f3f01c046.jpg?v=0"/></body></html>';
+		$html = '<html><body><img src="http://farm4.static.flickr.com/3408/3575435148_80e4a00b19.jpg?v=0"/></body></html>';
 		$images = p2mixi_extract_jpeg_images ( $html, 1 );
-		Assert::equals( count ($images['images']), 1, '');
-		Assert::equals( strlen ($images['images'][0]), 39075, '');
+//		Assert::equals( count ($images['images']), 1, '');
+		Assert::equals( strlen ($images['images'][0]), 5221, '');
+	}
+	function testExtractJpegImagesWithRedirection () {
+		$html = '<html><body><img src="http://ocafe.appspot.com/?redirect"/></body></html>';
+		$images = p2mixi_extract_jpeg_images ( $html, 1 );
+//		Assert::equals( count ($images['images']), 1, '');
+		Assert::equals( strlen ($images['images'][0]), 5221, '');
 	}
 }
 ?>
