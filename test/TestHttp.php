007935b0dbb838a3ce0313bdb8b4492dc173cf4c
Kei Saito <kei.saito@gmail.com>
2009-06-07 00:05:18 -0700

    Updated testcase

--- test/TestHttp.php
+++ test/TestHttp.php
@@ -5,38 +5,44 @@ class TestHttp extends Test {
 	
 	
 	function testGet () {
-		$url = 'http://farm4.static.flickr.com/3408/3575435148_80e4a00b19.jpg?v=0';
+		$url = $this->getTestImageURL();
 		$response_headers = array();
 		$response_body = '';
 		p2mixi_http_get( $url, array(), $response_headers, $response_body );
 		Assert::equals( strlen ( $response_body ), 5221, '');
 	}
 	function testGetRedirection () {
-		// This URL redirects to 'http://farm4.static.flickr.com/3408/3575435148_80e4a00b19.jpg?v=0'
-		$url = 'http://ocafe.appspot.com/?redirect=true';  
+		$url = $this->getTestServerURL() . '?redirect=true';  
 		$response_headers = array();
 		$response_body = '';
 		p2mixi_http_get( $url, array(), $response_headers, $response_body );
 		Assert::equals( strlen ( $response_body ), 5221, '');
 	}
-//	function testGetRedirectionWithCookies () {
-//		// This URL redirects to 'http://farm4.static.flickr.com/3408/3575435148_80e4a00b19.jpg?v=0'
-//		// only if the requst has the valid cookie "redirected=redirected".
-//		$url = 'http://ocafe.appspot.com/?redirecting-with-cookies=true';  
-//		$request_headers = array( 'Cookie'=>'redirected=redirected');
-//		$response_headers = array();
-//		$response_body = '';
-//		p2mixi_http_get( $url, $request_headers, $response_headers, $response_body );
-//		Assert::equals( strlen ( $response_body ), 5221, '');
-//	}
 	function testPost () {
-		// This URL just returns the post request body as is.
-		$url = 'http://ocafe.appspot.com/';
+		$url = $this->getTestServerURL();  
+		$request_headers = array( 'Content-type'=>'application/x-www-form-urlencoded');
 		$response_headers = array();
 		$response_body = '';
-		$request_body = 'myparam=myvalue';
-		p2mixi_http_post( $url, array(), $request_body, $response_headers, $response_body );
+		$request_body = 'name=value123';
+		p2mixi_http_post( $url, $request_headers, $request_body, $response_headers, $response_body );
 		Assert::equals( $response_body, $request_body, $request_body );
+	}
+	
+	function getTestServerPath()
+	{
+		$url = 'http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['REQUEST_URI'];
+		$url = substr( $url, 0, strrpos( $url, '/') );
+		$url = substr( $url, 0, strrpos( $url, '/') );
+		$url = $url . '/test-server/';
+		return $url;
+	}
+	function getTestServerURL()
+	{
+		return $this->getTestServerPath() . 'TestServer.php';
+	}
+	function getTestImageURL()
+	{
+		return $this->getTestServerPath() . 'test.jpg';
 	}
 }
 ?>
