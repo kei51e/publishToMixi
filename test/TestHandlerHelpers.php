ed4d4706048f89d72a711b85eab70a96c4cfdacb
Kei Saito <kei.saito@gmail.com>
2009-06-13 02:31:26 -0700

    Add more test cases

--- test/TestHandlerHelpers.php
+++ test/TestHandlerHelpers.php
@@ -4,6 +4,7 @@ require_once( '../publishToMixi.php' );
 class TestHandlerHelpers extends Test {
 	function doTest ( $testfunc, $input, $expected, $arg = NULL, $message = '' ) {
 		$actual = $testfunc( $input, $arg );
+//		echo ( $actual );
 		Assert::equals( $actual, $expected, $message );
 	}
 
@@ -35,5 +36,57 @@ class TestHandlerHelpers extends Test {
 		$out = '<br/>www.example.com\n<hr/>foo';
 		$this->doTest( 'p2mixi_replace_hyperlinks', $in, $out, array( $ignore ) );
 	}
+	
+	function testSanitizeYoutubeEmbed () {
+		$in = 'abc<object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/Krw7DJL69c8&hl=en&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/Krw7DJL69c8&hl=en&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="344"></embed></object>def';
+		$out = 'abc def';
+		$this->doTest( 'p2mixi_sanitize_html', $in, $out );
+		
+	}
+	function testSanitizeAmazonAffiliateImageLink () {
+		$in = 'abc<iframe src="http://rcm-jp.amazon.co.jp/e/cm?t=ksnn-22&o=9&p=8&l=as1&asins=4840233616&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>def';
+		$out = "abc\n\ndef";
+		$this->doTest( 'p2mixi_sanitize_html', $in, $out );
+	}	
+	function testSanitizeAmazonAffiliateTextLink () {
+		$in = 'abc<a href="http://www.amazon.co.jp/gp/product/4840233616?ie=UTF8&tag=ksnn-22&linkCode=as2&camp=247&creative=1211&creativeASIN=4840233616">図書館戦争</a><img src="http://www.assoc-amazon.jp/e/ir?t=ksnn-22&l=as2&o=9&a=4840233616" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />def';
+		$out = "abc図書館戦争def";
+		$this->doTest( 'p2mixi_sanitize_html', $in, $out );
+	}	
+	
+	function testParseYoutubeEmbed () {
+//		$in = 'abc<object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/Krw7DJL69c8&hl=en&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/Krw7DJL69c8&hl=en&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="344"></embed></object>def';
+		$in = '<object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/FOFwN_6vAQc&hl=en&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/FOFwN_6vAQc&hl=en&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="344"></embed></object>';
+		$out = '&lt;externalvideo src="YT:FOFwN_6vAQc"&gt;';
+		$this->doTest( 'p2mixi_parse_movie_links', $in, $out );
+	}
+	function testParseYoutubeLink () {
+		$in = 'abc<a href="http://www.youtube.com/watch?v=FOFwN_6vAQc">de</a>f';
+		$out = '&lt;externalvideo src="YT:FOFwN_6vAQc"&gt;';
+		$this->doTest( 'p2mixi_parse_movie_links', $in, $out );
+	}
+	function testParseNicovideoEmbed () {
+		$in = 'abc<iframe width="312" height="176" src="http://ext.nicovideo.jp/thumb/sm137256" scrolling="no" style="border:solid 1px #CCC;" frameborder="0"><a href="http://www.nicovideo.jp/watch/sm137256">【ニコニコ動画】一度は耳にしたことがある洋楽集</a></iframe>';
+		$out = '&lt;externalvideo src="NC:sm137256"&gt;';
+		$this->doTest( 'p2mixi_parse_movie_links', $in, $out );
+		
+	}
+	function testParseNicovideoLink () {
+		$in = 'abc<a target="_blank" href="http://www.nicovideo.jp/watch/sm137256">【ニコニコ動画】一度は耳にしたことがある洋楽集</a>';
+		$out = '&lt;externalvideo src="NC:sm137256"&gt;';
+		$this->doTest( 'p2mixi_parse_movie_links', $in, $out );
+	}
+	function testParseMovieLinkCombinations () {
+		$in = '<object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/FOFwN_6vAQc&hl=en&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/FOFwN_6vAQc&hl=en&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="344"></embed></object>';
+		$in .= 'abc<a href="http://www.youtube.com/watch?v=FOFwN_6vAQc">de</a>f';
+		$in .= 'abc<iframe width="312" height="176" src="http://ext.nicovideo.jp/thumb/sm137256" scrolling="no" style="border:solid 1px #CCC;" frameborder="0"><a href="http://www.nicovideo.jp/watch/sm137256">【ニコニコ動画】一度は耳にしたことがある洋楽集</a></iframe>';
+		$in .= 'abc<a target="_blank" href="http://www.nicovideo.jp/watch/sm137256">【ニコニコ動画】一度は耳にしたことがある洋楽集</a>';
+		$out = '&lt;externalvideo src="YT:FOFwN_6vAQc"&gt;';
+		$out .= '&lt;externalvideo src="YT:FOFwN_6vAQc"&gt;';
+		$out .= '&lt;externalvideo src="NC:sm137256"&gt;';
+		$out .= '&lt;externalvideo src="NC:sm137256"&gt;';
+		$this->doTest( 'p2mixi_parse_movie_links', $in, $out );
+		
+	}	
 }
 ?>
